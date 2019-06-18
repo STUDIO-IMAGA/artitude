aa298cb6ce5004d79182acf179c7f0401ff75a69
odil-io <github@odil.io>
2019-06-18 17:07:46 +0200

    Edit #8: page header
    remove empty elements and remove padding top.

--- templates/header.php
+++ templates/header.php
@@ -5,22 +5,25 @@
 <? $text = get_field('header_text'); ?>
 
 <? $order = ( is_front_page() ) ? "order-sm-3" : "order-sm-1"; ?>
+<? $empty = ( $text and $image ) ? "header" : "header empty"; ?>
 
 
-<section class="element header">
-  <div class="container">
-    <div class="row align-items-stretch">
-      <div class="col-12 col-sm-6 order-2 <?= $order; ?>">
-        <div class="header-image">
-          <div class="header-image-bg" data-aos="fade-left"></div>
-          <img class="img-fluid" src="<?= $image['sizes']['header-image']; ?>" data-aos="fade-right" />
+<section class="element <?= $empty; ?>">
+  <? if( $text and $image ): ?>
+    <div class="container">
+      <div class="row align-items-stretch">
+        <div class="col-12 col-sm-6 order-2 <?= $order; ?>">
+          <div class="header-image">
+            <div class="header-image-bg" data-aos="fade-left"></div>
+            <img class="img-fluid" src="<?= $image['sizes']['header-image']; ?>" data-aos="fade-right" />
+          </div>
         </div>
-      </div>
-      <div class="col-12 col-sm-6 order-1 order-sm-2 border border-white" data-aos="fade-left">
-        <div class="content">
-          <?= $text; ?>
+        <div class="col-12 col-sm-6 order-1 order-sm-2 border border-white" data-aos="fade-left">
+          <div class="content">
+            <?= $text; ?>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  <? endif; ?>
 </section>
