7441b605cd9dd1caf6d084fc7b11d5a9083bbfba
odil-io <github@odil.io>
2019-11-21 17:14:46 +0100

    Add: gallery option to theme layout

--- templates/elements/content-themes.php
+++ templates/elements/content-themes.php
@@ -3,20 +3,28 @@
     <div class="row align-items-center">
       <div class="col-12 themes">
         <div class="row justify-content-center">
-          <? $themes = get_sub_field('themes'); ?>
-          <? foreach( $themes as $theme ): ?>
-            <? $image = $theme['image']; ?>
-            <div class="col-12 col-sm-6 col-md-4 theme">
-              <a class="lightbox" href="<?= $image['url']; ?>">
-                <div class="text-center">
-                  <img class="img-fluid" src="<?= $image['sizes']['gallery-small']; ?>" alt="<?= $image['alt']; ?>" />
-                </div>
-                <div class="title">
-                  <?= $theme['title']; ?>
-                </div>
-              </a>
+          <?php $themes = get_sub_field('themes'); $i=0;?>
+          <?php foreach( $themes as $theme ): $i++; $array = "";?>
+            <?php $gallery = $theme['image']; ?>
+            <?php $thumbnail_url = wp_get_attachment_image_url( $gallery[0], 'gallery-small' ); ?>
+            <?php $full_url = wp_get_attachment_image_url( $gallery[0], 'full' ); ?>
+            <?php foreach($gallery as $image): ?>
+              <?php $array .= "'".wp_get_attachment_image_url( $image, 'full' )."',"; ?>
+            <?php endforeach; ?>
+            <div class="col-12 col-sm-6 col-md-4 theme lightbox-<?php echo $gallery[0]; ?>">
+              <div class="gallery-container">
+                <img class="img-fluid" src="<?php echo $thumbnail_url; ?>" alt="<?php echo $theme['title']; ?>" />
+                <div class="gallery-paging"><?php echo count($gallery); ?><span class="dashicons dashicons-images-alt2"></span></div>
+              </div>
+              <div class="title">
+                <?php echo $theme['title']; ?>
+              </div>
             </div>
-          <? endforeach; ?>
+              <?php
+              // Very dirty inline.. oops
+              wp_add_inline_script('jquery', "$('.lightbox-".$gallery[0]."').on('click',function(){SimpleLightbox.open({items: [".$array."]});});");
+            ?>
+          <?php endforeach; ?>
         </div>
       </div>
     </div>
